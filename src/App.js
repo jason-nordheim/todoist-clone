dbb28bd026e8d049c044377bbefdb15672499e05
Jason Nordheim <jason.nordheim@gmail.com>
2021-04-16 16:39:52 -0600

    imported stylesheet using node-sass

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Content } from "./components/layout/Contents";
 import { Header } from "./components/layout/Header";
+import "./App.scss"; // using node-sass
 
 function App() {
   return (
