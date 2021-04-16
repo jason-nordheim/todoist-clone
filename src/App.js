180076bc9f6f84ed42e045cde6632fac8befcf7b
Jason Nordheim <jason.nordheim@gmail.com>
2021-04-16 15:49:58 -0600

    stamped out <App /> component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import { Content } from "./components/layout/Contents";
+import { Header } from "./components/layout/Header";
+
 function App() {
   return (
     <div className="App">
-      <p>Hello there</p>
+      <Header />
+      <Content />
     </div>
   );
 }
