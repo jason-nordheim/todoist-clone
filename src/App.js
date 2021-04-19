7ecf90b25af1b362b6b013e7c4ebca9a1013c625
Jason Nordheim <jason.nordheim@gmail.com>
2021-04-19 10:55:44 -0600

    wrapped in context

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import { Content } from "./components/layout/Contents";
 import { Header } from "./components/layout/Header";
 import "./App.scss"; // using node-sass
+import { ProjectsProvider, SelectedProjectsProvider } from "./context";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Content />
-    </div>
+    <SelectedProjectsProvider>
+      <ProjectsProvider>
+        <div className="App">
+          <Header />
+          <Content />
+        </div>
+      </ProjectsProvider>
+    </SelectedProjectsProvider>
   );
 }
 
